5607291bbb324d7d33be00fb6303829fafe768f2
poing <brian.lavallee@invite-comm.jp>
2018-11-21 14:08:38 +0900

    Auto julian Date

--- docs/conf.py
+++ docs/conf.py
@@ -194,7 +194,7 @@ latex_elements = {
 'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-'pointsize': '12pt',
+'pointsize': '11pt',
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
@@ -214,7 +214,7 @@ latex_logo = 'images/INVITE-Original_Combined.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+latex_use_parts = False
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
