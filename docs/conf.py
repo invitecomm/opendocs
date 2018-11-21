7091f11a270ff12d2b9533dcb9da097e55575ff2
poing <brian.lavallee@invite-comm.jp>
2018-11-21 17:03:26 +0900

    xelatex

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ from datetime import date
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = []
-extensions = ['sphinxcontrib.cjk']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
